2d9e505999170bc848b8b4829846d89db5248e54
vchernousov <vchernousov@logrocon.com>
2021-08-01 18:32:07 +0300

    add firebase auth

--- logroocn_chat/lib/main.dart
+++ logroocn_chat/lib/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:logroocn_chat/pages/home_page.dart';
+import 'package:logroocn_chat/pages/root_page.dart';
+import 'package:logroocn_chat/services/authentification.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  static const primarySwatch = Colors.green;
+  // button color
+  static const buttonColor = Colors.green;
+  // app name
+  static const appName = 'Logrocon Chat';
+  // boolean for showing home page if user unverified
+  static const homePageUnverified = false;
+
+  final params = {
+    'appName': appName,
+    'primarySwatch': primarySwatch,
+    'buttonColor': buttonColor,
+    'homePageUnverified': homePageUnverified,
+  };
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: appName,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: new RootPage(
+        params: params,
+        auth: new Auth(),
+      ),
     );
   }
 }
